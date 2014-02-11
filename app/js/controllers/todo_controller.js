2d86396c56a1f4ae0a0157b2181f44f0aff947cd
Mark Tareshawty <tarebyte@gmail.com>
2014-02-11 02:12:02 -0500

    delete todo item

--- app/js/controllers/todo_controller.js
+++ app/js/controllers/todo_controller.js
@@ -12,6 +12,12 @@ Todos.TodoController = Ember.ObjectController.extend({
       } else {
         this.get('model').save();
       }
+    },
+
+    removeTodo: function() {
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
     }
   },
 
