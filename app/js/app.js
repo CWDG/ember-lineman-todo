90ce427dfa5744b82c45f98a283d04ccd972e8a3
Mark Tareshawty <tarebyte@gmail.com>
2014-02-10 23:24:55 -0500

    added todos markup

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,3 @@ App = Ember.Application.create();
 App.Router.map(function() {
   // put your routes here
 });
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
